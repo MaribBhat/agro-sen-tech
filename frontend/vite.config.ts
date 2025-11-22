8b12f89c858c5c3b20b771883dc61fa944afb676
Marib Bhat <104448477+MaribBhat@users.noreply.github.com>
2025-11-22 22:02:25 +0530

    Add files via upload

---
+++ frontend/vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  build: {
+    outDir: "dist", // Default is 'dist', but you can change it to 'build', 'output', etc.
+  },
+  server: {
+    host: "::",
+    port: 8080,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:9000",
+        changeOrigin: true,
+      },
+    },
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean,
+  ),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
